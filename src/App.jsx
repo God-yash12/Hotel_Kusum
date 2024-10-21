30566af5ed5ee205580bee07a49609d7f8f0eaa4
bjsuraj034 <itsmesuraj034@gmail.com>
2024-10-21 13:48:15 +0545

    "mero conflict solve vayoo anii balla gardai xuu hai"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Nav from './components/Nav'
+import Routing from './Routes/Routing'
 
 const App = () => {
   return (
     <>
-    <Nav/>
+    <Routing/>
     
     </>
   )
